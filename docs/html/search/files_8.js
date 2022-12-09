77df228e9704a8eac4996d4d086b44d0c8caab7f
kartikrajeshwaran <kartikrajeshwaran.kr@gmail.com>
2022-12-09 00:52:57 -0500

    Minor layout adjustment.

--- docs/html/search/files_8.js
+++ docs/html/search/files_8.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['license_2emd_0',['LICENSE.md',['../_l_i_c_e_n_s_e_8md.html',1,'']]],
-  ['loss_5ffunctions_2emd_1',['loss_functions.md',['../loss__functions_8md.html',1,'']]],
-  ['lr_5fschedulers_2emd_2',['lr_schedulers.md',['../lr__schedulers_8md.html',1,'']]]
+  ['loss_5ffunctions_2emd_0',['loss_functions.md',['../loss__functions_8md.html',1,'']]],
+  ['lr_5fschedulers_2emd_1',['lr_schedulers.md',['../lr__schedulers_8md.html',1,'']]]
 ];
--- docs/html/search/pages_4.js
+++ docs/html/search/pages_4.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['learning_20rate_20schedulers_0',['Learning Rate Schedulers',['../lr_schedulers.html',1,'']]],
-  ['license_1',['LICENSE',['../md___users_kartikrajeshwaran__library__cloud_storage__google_drive_kartikrajeshwaran_kr_gmail_co91e05b716960562cf6593ac5ebaf09a4.html',1,'']]],
-  ['loss_20functions_2',['Loss Functions',['../loss_functions.html',1,'']]]
+  ['loss_20functions_1',['Loss Functions',['../loss_functions.html',1,'']]]
 ];
