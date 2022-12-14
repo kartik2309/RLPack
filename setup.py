b18713e55cc4cffd5e26bcaa89be0bd06bd6ddfe
kartikrajeshwaran <kartikrajeshwaran.kr@gmail.com>
2022-12-13 22:03:51 -0500

    Update version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 from torch.utils import cmake_prefix_path
 
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 
 
 class CMakeExtension(Extension):
