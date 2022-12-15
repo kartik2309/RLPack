e205b2169ef42762e6f4f3fcdbb549926439b119
kartikrajeshwaran <kartikrajeshwaran.kr@gmail.com>
2022-12-15 01:24:55 -0500

    Updated documentation.

---
+++ docs/html/classrlpack_1_1___c_1_1grad__accumulator_1_1_grad_accumulator.js
@@ -0,0 +1,9 @@
+var classrlpack_1_1___c_1_1grad__accumulator_1_1_grad_accumulator =
+[
+    [ "__init__", "classrlpack_1_1___c_1_1grad__accumulator_1_1_grad_accumulator.html#aa9efa4c6f18763846177f16d5b7f52ce", null ],
+    [ "accumulate", "classrlpack_1_1___c_1_1grad__accumulator_1_1_grad_accumulator.html#abb7969848a77ce453feaed02d92e7a53", null ],
+    [ "clear", "classrlpack_1_1___c_1_1grad__accumulator_1_1_grad_accumulator.html#aeb0009d5af07526a1e5a6e99cbbc6120", null ],
+    [ "mean_reduce", "classrlpack_1_1___c_1_1grad__accumulator_1_1_grad_accumulator.html#ac17fc3bee0ce36b494ed11c59532119d", null ],
+    [ "c_grad_accumulator", "classrlpack_1_1___c_1_1grad__accumulator_1_1_grad_accumulator.html#a09124dcb529ca95effd9e5b7f1014a4b", null ],
+    [ "map_of_tensors", "classrlpack_1_1___c_1_1grad__accumulator_1_1_grad_accumulator.html#a62ce1f844ff5146af2f7e40e4b217255", null ]
+];
---
+++ docs/html/group__grad__accumulator__group.js
@@ -0,0 +1,14 @@
+var group__grad__accumulator__group =
+[
+    [ "C_GradAccumulator", "class_c___grad_accumulator.html", [
+      [ "C_GradAccumulator", "class_c___grad_accumulator.html#abe04783b053cdaa87a490377faac89d8", null ],
+      [ "~C_GradAccumulator", "class_c___grad_accumulator.html#ac99d800fb7cc9eca53cdf7bea982ac04", null ],
+      [ "accumulate", "class_c___grad_accumulator.html#a696017b1ed9c6a3e5bccccecd9a73e10", null ],
+      [ "clear", "class_c___grad_accumulator.html#a2b3b3befbbf516dadbea8676bbcaf52f", null ],
+      [ "mean_reduce", "class_c___grad_accumulator.html#a5171e9bc7b55f23e179fee896c946a69", null ],
+      [ "bootstrapRounds_", "class_c___grad_accumulator.html#a3cb7289bd1dcd654ab0343de19fd8186", null ],
+      [ "namedParametersGrads_", "class_c___grad_accumulator.html#ad59f4483058644f047b7d60381649c23", null ],
+      [ "parameterKeys_", "class_c___grad_accumulator.html#ae8f64a55b669647e78887385540ebd64", null ]
+    ] ],
+    [ "PYBIND11_MODULE", "group__grad__accumulator__group.html#ga6c6f30d5d2df9f96f907debc36b7a701", null ]
+];
