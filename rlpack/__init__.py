01078957fb5375d1a7f3106d71cd395e24f68ca2
kartikrajeshwaran <kartikrajeshwaran.kr@gmail.com>
2022-12-30 01:05:27 -0500

    Move `get_site_packages` to __init__.py.

--- rlpack/__init__.py
+++ rlpack/__init__.py
@@ -4,6 +4,9 @@
 been optimized with C++ backend.
 """
 
+# Import other packages
+from site import getsitepackages
+
 # Import pytorch packages
 import torch as pytorch
 import torch.distributed as pytorch_distributed
@@ -13,3 +16,12 @@ from torch.utils.tensorboard import SummaryWriter
 
 # Import CPP Backend
 from rlpack.lib import C_GradAccumulator, C_ReplayBuffer, C_RolloutBuffer
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Function Definitions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+def get_prefix_path():
+    """
+    Gets prefix path for rlpack package, from python installation.
+    @return: str: The prefix path to rlpack.
+    """
+    return f"{getsitepackages()[0]}/rlpack"
